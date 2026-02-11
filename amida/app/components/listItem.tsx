fcd8c2cfeef6e87fbd00a91c522d1cedf41775e4
Kusa Mochi <whoatemyapplepie@gmail.com>
2026-02-11 19:31:21 +0900

    文字列を多言語化

--- amida/app/components/listItem.tsx
+++ amida/app/components/listItem.tsx
@@ -2,6 +2,7 @@
 
 import { FC } from "react";
 import { DeleteIcon } from "./icons/deleteIcon";
+import { useTranslation } from "react-i18next";
 
 type Props = {
     label: string;
@@ -10,10 +11,11 @@ type Props = {
 }
 
 export const ListItem: FC<Props> = ({ label, onChange, onDelete }) => {
+    const { t } = useTranslation();
     return (
         <div className="flex flex-row flex-nowrap justify-start items-center">
             <div className="w-full p-2 mr-3 rounded-md shadow-md border border-gray-300">
-                <input type="text" defaultValue={label} className="w-full outline-none" placeholder="あみだくじの選択肢を書いてください" onChange={(e) => onChange?.(e.target.value)} />
+                <input type="text" defaultValue={label} className="w-full outline-none" placeholder={t("itemPlaceholder")} onChange={(e) => onChange?.(e.target.value)} />
             </div>
             <DeleteIcon onClick={onDelete} />
         </div>
