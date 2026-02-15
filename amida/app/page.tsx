a24130081f9a9a30255da84ed679b0c04284452b
Kusa Mochi <whoatemyapplepie@gmail.com>
2026-02-15 15:24:14 +0900

    ページ全体のパディングを調整

--- amida/app/page.tsx
+++ amida/app/page.tsx
@@ -30,7 +30,7 @@ export default function Home() {
   return (
     <GoalsContext.Provider value={{ goals, setGoals }}>
       <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-        <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between p-16 bg-white dark:bg-black sm:items-start">
+        <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between p-8 bg-white dark:bg-black sm:items-start">
           {initVisible && <Init gotoAmida={gotoAmida} />}
           {amidaVisible && <Amida gotoInit={gotoInit} />}
         </main>
